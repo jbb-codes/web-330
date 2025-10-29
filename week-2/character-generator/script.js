7489ce3a9d4714f3e204bd76f3167ca5d22957d7
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2025-10-28 23:51:01 -0400

    chore(week-2): completed character-generator assignment

---
+++ week-2/character-generator/script.js
@@ -0,0 +1,49 @@
+/*
+  Pragmatic JavaScript
+  Chapter 1
+  Programming Assignment
+
+  Author: Jarren Bess
+  Date: Oct 28, 2025
+  Filename:
+*/
+
+"use strict";
+
+function createCharacter(name, gender, characterClass) {
+  function getName() {
+    return name;
+  }
+
+  function getGender() {
+    return gender;
+  }
+
+  function getClass() {
+    return characterClass;
+  }
+
+  return { getName, getGender, getClass };
+}
+
+document.getElementById("generateHero").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const heroName = document.querySelector("#heroName").value;
+  const heroGender = document.querySelector("#heroGender").value;
+  const heroClass = document.querySelector("#heroClass").value;
+
+  const character = createCharacter(heroName, heroGender, heroClass);
+
+  document.querySelector(
+    ".charName"
+  ).textContent = `Name: ${character.getName()}`;
+
+  document.querySelector(
+    ".charGender"
+  ).textContent = `Gender: ${character.getGender()}`;
+
+  document.querySelector(
+    ".charClass"
+  ).textContent = `Class: ${character.getClass()}`;
+});
