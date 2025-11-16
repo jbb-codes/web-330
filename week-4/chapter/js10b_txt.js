b977ba5aa9c67dae896e113fe02bf4db65239880
Jarren Bess <7104202+jbb-codes@users.noreply.github.com>
2025-11-16 01:13:52 -0500

    feat(week-4): add chapter follow along and hands-on project10-01

---
+++ week-4/chapter/js10b_txt.js
@@ -0,0 +1,18 @@
+"use strict";
+/*  JavaScript 7th Edition
+    Chapter 10
+    Chapter case
+
+    Oak Top House Directions
+    Author: Jarren Bess
+    Date: Nov 15, 2025
+
+    Filename: js10b.js
+*/
+
+// Function to set up and display the Oak Top House Map
+function initMap() {
+  // Page objects
+  let displayMap = document.getElementById("displayMap");
+  let routeBox = document.getElementById("routeBox");
+}
